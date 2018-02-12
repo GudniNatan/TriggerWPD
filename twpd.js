54703360f882d33b007c45e4569c3d981fbc5723
Guðni Natan Gunnarsson <1493259+GudniNatan@users.noreply.github.com>
2018-02-12 15:45:15 +0000

    initial

---
+++ twpd.js
@@ -0,0 +1,39 @@
+(async function() {
+	let xhttp = new XMLHttpRequest();
+	let btn = document.getElementById('submitButton');
+	let url = document.getElementById('url');
+	function sleep(ms) {
+  		return new Promise(resolve => {
+    		setTimeout(() => {
+      			resolve('resolved');
+    		}, ms);
+  		});
+	}
+
+	async function breaker(url) {
+		// body...
+		await sleep(2000);
+		try{
+			var i = document.createElement("img");
+			i.src = url + Math.floor(Math.random() * 200);
+		}
+		catch(err){
+			console.log(err);
+		}
+	}
+	btn.addEventListener("click", function(event) {
+		event.preventDefault();
+		let val = url.value;
+		if (val[val.length - 1] !== '/')
+		{
+			val += "/foo/bar/";
+		}
+		else{
+			val += "foo/bar/";
+		}
+		for (var i = 0; i < 20; i++) {
+			breaker(val);
+		}
+	});
+})();
+
